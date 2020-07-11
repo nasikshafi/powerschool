4d50ffd0a49a8a909eedb7bbcb21e6ad83515a2f
NasikShafi <nasikshafi@gmail.com>
2020-07-11 11:11:31 +0530

    initial commit

---
+++ working.js
@@ -0,0 +1,33 @@
+
+const csv=require('csvtojson')
+const converter=csv()
+.fromFile('./country.csv')
+.then((json)=>{
+  console.log(json)
+    countries =[];
+    countries=json;
+})
+ 
+  
+  const searchInput = document.querySelector('.search-input');
+  const suggestionsPanel = document.querySelector('.suggestions');
+  
+  searchInput.addEventListener('keyup', function() {
+    const input = searchInput.value;
+    suggestionsPanel.innerHTML = '';
+    const suggestions = countries.filter(function(country) {
+      return country.name.toLowerCase().startsWith(input);
+    });
+    suggestions.forEach(function(suggested) {
+      const div = document.createElement('div');
+      div.innerHTML = suggested.name;
+      suggestionsPanel.appendChild(div);
+    });
+    if (input === '') {
+      suggestionsPanel.innerHTML = '';  
+    }
+  })
+  
+  
+  
+  
